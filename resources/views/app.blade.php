53b5cc7f695e6eaa6e40801248ef1ac28916d113
Alifffu <2410817310013@mhs.ulm.ac.id>
2026-06-17 13:23:04 +0800

    login page dan rename ke SIJAMAL

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     
-    <title>Sistem Peminjaman Ruangan - SIMARI</title>
+    <title>SIJAMAL - Sistem Informasi Jadwal dan Manajemen Alokasi Ruangan</title>
 
     <!-- Google Fonts: Instrument Sans -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
